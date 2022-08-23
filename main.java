6897fd196d08428dabf08abd8470d5238b77aaf1
Elena <111504533+Ellena2022@users.noreply.github.com>
2022-08-23 14:20:31 +0300

    Add files via upload

---
+++ main.java
@@ -0,0 +1,10 @@
+public class main {
+    public static void main (String [] args) {
+        for (int i=1; i<11; i++) {
+            int q = 7;
+            {
+                System.out.println(i+"*"+q+"="+i*q);
+            }
+        }
+    }
+}
